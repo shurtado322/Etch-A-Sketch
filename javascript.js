16dcb4dec94c13c09d61ab3c2c786233cf2999c0
shurtado322 <sergiohurtado322@gmail.com>
2021-06-01 16:37:03 -0700

    Working on javascript file to implement the interactivity of the Etch a sketch.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,33 @@
 const grid = document.querySelector(".gridContainer");
 const userInput = document.getElementById("quantity");
 const resetButton = document.querySelector(".reset");
+
+createGrid = () => {
+    for (let i = 0; i < 256; i++){
+        const div = document.createElement("div");
+        div.classList.add("square");
+        grid.appendChild(div);
+    }
+};
+
+updateGrid = () => {
+    grid.innerHTML = "";
+    grid.style.setProperty("grid-template-columns",
+        `repeat(${userInput.value}, 2fr)`);
+    
+    grid.style.setProperty("grid-template-rows",
+        `repeat(${userInput.value}, 2fr)`);
+
+    for (let i = 0; i < userInput.value * userInput.value; i++){
+        const div = document.createElement("div");
+        div.classList.add("squre");
+        grid.appendChild(div);
+    }
+    console.log(userInput.value);
+};
+
+const square = document.querySelector("div");
+square.addEventListener("mouseover", function(event) {
+    event.target.classList.replace("square", "color");
+});
+
